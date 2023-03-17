f091590d4bf25eebad355e14767ffb54dda8e4da
Sviatoslav <slavaua2312@gmail.com>
2023-03-17 12:59:38 +0200

    Add some functional

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
-import com.example.myfirstapp.Shape.Line
-import com.example.myfirstapp.Shape as Shape1
+import com.example.myfirstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Toast.makeText(this, "Text", Toast.LENGTH_SHORT).show()
+        with(binding) {
+            binding.button.setOnClickListener {
+                var newText = binding.et.text.toString()
+                binding.tvHi.text = newText
+
+
+                Toast.makeText(this@MainActivity, "Button pressed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
 
